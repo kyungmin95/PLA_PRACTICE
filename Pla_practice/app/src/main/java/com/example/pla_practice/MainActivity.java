f76519647783d0b934383f73cd3898689d67e236
kyungmin95 <kkmini395@gmail.com>
2017-05-28 21:23:48 +0900

    Make Tab 01
    make Tab_
    only make tab.
    content 내용 넣는 것 xml 사용.

--- Pla_practice/app/src/main/java/com/example/pla_practice/MainActivity.java
+++ Pla_practice/app/src/main/java/com/example/pla_practice/MainActivity.java
@@ -1,25 +1,32 @@
 package com.example.pla_practice;
 
+import android.app.TabActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTabHost;
-import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.widget.TabHost;
 
-public class MainActivity extends ActionBarActivity {
-
-    FragmentTabHost tabHost;
+public class MainActivity extends TabActivity {
+    TabHost mTab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tabHost = (FragmentTabHost)findViewById(R.id.tabhost);
-        tabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
-        tabHost.addTab(tabHost.newTabSpec("home").setIndicator("",getResources().getDrawable(R.drawable.home)), FragmentOne.class, null);
-        tabHost.addTab(tabHost.newTabSpec("month").setIndicator("",getResources().getDrawable(R.drawable.month)), FragmentOne.class, null);
-        tabHost.addTab(tabHost.newTabSpec("week").setIndicator("",getResources().getDrawable(R.drawable.week)), FragmentOne.class, null);
-        tabHost.addTab(tabHost.newTabSpec("day").setIndicator("",getResources().getDrawable(R.drawable.day)), FragmentOne.class, null);
-        tabHost.addTab(tabHost.newTabSpec("memo").setIndicator("",getResources().getDrawable(R.drawable.memo)), FragmentOne.class, null);
-        tabHost.addTab(tabHost.newTabSpec("pen").setIndicator("",getResources().getDrawable(R.drawable.pen)), FragmentOne.class, null);
+        //setContentView(R.layout.activity_main);
+        TabHost mTab = getTabHost();
+        TabHost.TabSpec spec;
+        LayoutInflater.from(this).inflate(R.layout.activity_main, mTab.getTabContentView(), true);
 
+        spec = mTab.newTabSpec("home").setIndicator("", getResources().getDrawable(R.drawable.home)).setContent(R.id.tv1);
+        mTab.addTab(spec);
+        spec = mTab.newTabSpec("month").setIndicator("", getResources().getDrawable(R.drawable.month)).setContent(R.id.tv1);
+        mTab.addTab(spec);
+        spec = mTab.newTabSpec("week").setIndicator("", getResources().getDrawable(R.drawable.week)).setContent(R.id.tv1);
+        mTab.addTab(spec);
+        spec = mTab.newTabSpec("day").setIndicator("", getResources().getDrawable(R.drawable.day)).setContent(R.id.tv1);
+        mTab.addTab(spec);
+        spec = mTab.newTabSpec("memo").setIndicator("", getResources().getDrawable(R.drawable.memo)).setContent(R.id.tv1);
+        mTab.addTab(spec);
+        spec = mTab.newTabSpec("pen").setIndicator("", getResources().getDrawable(R.drawable.pen)).setContent(R.id.tv1);
+        mTab.addTab(spec);
         //for(int i = 0; i < tabHost.getTabWidget().getChildCount(); i++) {
         //    tabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("7FA3CF"));
        // }
