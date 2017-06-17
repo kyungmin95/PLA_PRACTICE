37d50f3b9b40c277b6e78c4f93867d621a200ce0
kyungmin95 <kkmini395@gmail.com>
2017-06-17 22:04:27 +0900

    Make Pla_frag 01
    프래그먼트 활용했었던 탭 이용해서
    새로 탭 만들고 디자인 수정

---
+++ Pla_frag/app/src/main/java/com/example/pla_frag/DayFragment.java
@@ -0,0 +1,25 @@
+package com.example.pla_frag;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by 김경민 on 2017-06-17.
+ */
+
+public class DayFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        return inflater.inflate(R.layout.fragment_layout, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, Bundle saveInstanceState) {
+        super.onViewCreated(view, saveInstanceState);
+        TextView tv = (TextView)view.findViewById(R.id.textView1);
+        tv.setText("Hello");
+        }
+}
---
+++ Pla_frag/app/src/main/java/com/example/pla_frag/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.pla_frag;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends ActionBarActivity {
+
+    FragmentTabHost tabHost;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabHost = (FragmentTabHost)findViewById(R.id.tabhost);
+        tabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        //tabHost.addTab(tabHost.newTabSpec("home").setIndicator("",getResources().getDrawable(R.drawable.home)), DayFragment.class, null);
+        //tabHost.addTab(tabHost.newTabSpec("month").setIndicator("",getResources().getDrawable(R.drawable.month)), DayFragment.class, null);
+        //tabHost.addTab(tabHost.newTabSpec("week").setIndicator("",getResources().getDrawable(R.drawable.week)), DayFragment.class, null);
+        //tabHost.addTab(tabHost.newTabSpec("day").setIndicator("",getResources().getDrawable(R.drawable.day)), DayFragment.class, null);
+        //tabHost.addTab(tabHost.newTabSpec("memo").setIndicator("",getResources().getDrawable(R.drawable.memo)), DayFragment.class, null);
+        //tabHost.addTab(tabHost.newTabSpec("pen").setIndicator("",getResources().getDrawable(R.drawable.pen)), DayFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("home").setIndicator(getTabIndicator(tabHost.getContext(), R.drawable.home)), DayFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("month").setIndicator(getTabIndicator(tabHost.getContext(), R.drawable.month)), DayFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("week").setIndicator(getTabIndicator(tabHost.getContext(), R.drawable.week)), DayFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("day").setIndicator(getTabIndicator(tabHost.getContext(), R.drawable.day)), DayFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("memo").setIndicator(getTabIndicator(tabHost.getContext(), R.drawable.memo)), DayFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("pen").setIndicator(getTabIndicator(tabHost.getContext(), R.drawable.pen)), DayFragment.class, null);
+    }
+
+    private View getTabIndicator(Context context, int icon) {   //탭의 디자인을 지정하기 위해 탭 디자인 설정 layout과 연결시키는 함수
+        View view = LayoutInflater.from(context).inflate(R.layout.tab_layout, null);
+        ImageView iv = (ImageView) view.findViewById(R.id.imageView);
+        iv.setImageResource(icon);
+        return view;
+    }
+}
