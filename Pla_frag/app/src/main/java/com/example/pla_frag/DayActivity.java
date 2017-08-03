9f03b2210c2615d4b18e0ba1b0fdc877a70f95e6
kyungmin95 <kkmini395@gmail.com>
2017-08-03 23:22:26 +0900

    Pla_frag add Day
    탭이랑 데이 연결

--- Pla_frag/app/src/main/java/com/example/pla_frag/DayActivity.java
+++ Pla_frag/app/src/main/java/com/example/pla_frag/DayActivity.java
@@ -1,29 +1,44 @@
 package com.example.pla_frag;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import com.example.pla_frag.Day.DayAdapter;
+import com.example.pla_frag.Day.DayDB;
+import com.example.pla_frag.Day.DayDiaryActivity;
+import com.example.pla_frag.Day.DayDiaryDB;
+import com.example.pla_frag.Day.DayTodolistActivity;
 
-/**
- * Created by 김경민 on 2017-06-17.
- */
+import java.util.Calendar;
 
 public class DayActivity extends Activity {
     int year, month, day;
     TextView dayDate;
     Calendar c;
+    String dd;
+    DayAdapter todolistAA;
+    DayDB helper;
+    DayDiaryDB mHelper;
+    ListView todolistLV;
+    TextView dayMemo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.day_main);getCurrentDate();
+        setContentView(R.layout.day_main);
+        getCurrentDate();
         setDayDate();  //getCurrentDate로 오늘 날짜를 가져온 뒤, 그 값을 dayDate에 입력(아무 버튼도 누르지 않은 기본 상태)
 
+        //전날로 날짜를 옮기는 함수
         findViewById(R.id.day_left).setOnClickListener(new View.OnClickListener() { // '<' 버튼 눌렀을 때. 전날로 날짜 이동
             @Override
             public void onClick(View v) {
@@ -46,9 +61,12 @@ public class DayActivity extends Activity {
                     month = 12;
                 }
                 setDayDate();
+                setDayTodolist();
+                setDayMemo();
             }
         });
 
+        //다음날로 날짜를 옮기는 함수
         findViewById(R.id.day_right).setOnClickListener(new View.OnClickListener() {  // '>' 버튼 눌렀을 때. 다음날로 날짜 이동
             @Override
             public void onClick(View v) {
@@ -76,21 +94,56 @@ public class DayActivity extends Activity {
                     year += 1; month = 1;
                 }
                 setDayDate();
+                setDayTodolist();
+                setDayMemo();
             }
         });
 
+        //오늘로 날짜를 옮기는 함수
         findViewById(R.id.day_today).setOnClickListener(new View.OnClickListener() {  // '오늘' 버튼 눌렀을 때. 오늘로 날짜 이동
             @Override
             public void onClick(View v) {
                 getCurrentDate();
                 setDayDate();
+                setDayTodolist();
+                setDayMemo();
             }
         });
 
-        CheckBox dayCheck01 = (CheckBox)findViewById(R.id.day_check01);
-        CheckBox dayCheck02 = (CheckBox)findViewById(R.id.day_check02);
-        //if(dayCheck01.isChecked()) dayCheck01.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG); 아직 작동 안함
+        todolistAA = new DayAdapter();
+        //DB helper 생성
+        helper = new DayDB(this);
+        mHelper = new DayDiaryDB(this);
+        //리스트를 직접 구성한 이미지로 보이게 하기 위해 새로 만든 어댑터와 연결
+        todolistLV = (ListView)findViewById(R.id.daytodo_list);
+        setDayTodolist(); //함수 이용해 리스트 생성
+        todolistLV.setAdapter(todolistAA);
 
+        //to do list의 리스트(배경)를 선택하면 to do list 추가 화면으로 넘어가는 intent 설정
+        todolistLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent todolistIntent = new Intent(DayActivity.this, DayTodolistActivity.class);
+                todolistIntent.putExtra("year", year);
+                todolistIntent.putExtra("month", month);
+                todolistIntent.putExtra("day", day);
+                startActivityForResult(todolistIntent, 1);
+            }
+        });
+
+        //일기 부분 클릭하면 수정 부분으로 넘어가는 intent 설정
+        dayMemo = (TextView)findViewById(R.id.daymemo);
+        setDayMemo();
+        dayMemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent memoIntent = new Intent(DayActivity.this, DayDiaryActivity.class);
+                memoIntent.putExtra("year", year);
+                memoIntent.putExtra("month", month);
+                memoIntent.putExtra("day", day);
+                startActivityForResult(memoIntent, 1);
+            }
+        });
     }
 
     public void getCurrentDate() {  //오늘 날짜를 가져오는 함수. Calendar 사용.
@@ -100,23 +153,80 @@ public class DayActivity extends Activity {
         day = c.get(Calendar.DAY_OF_MONTH);
     }
 
-    public void setDayDate() {  //TextView에 년, 월, 일을 입력하는 함수
+    public void setDayDate() {  //TextView에 년, 월, 일을 입력하는 함수. 날짜를 가져옴과 동시에 DB에 검색에 쓰기 위한 dd도 함께 설정.
         dayDate = (TextView)findViewById(R.id.day_date);
         dayDate.setText(year+"년 "+month+"월 "+day+"일");
+        int syear = year; int smonth = month; int sday = day;
+        dd = Integer.toString(syear) + Integer.toString(smonth) + Integer.toString(sday);
     }
 
-    //public void setDayCheckBox(CheckBox cb, String str, String cbid) {  //DB를 사용한다면 쓰려 했던 체크박스 추가하는 함수인데, 일단 냅두자
+    public void mChangeDate(View v) {  //<> 표시 말고도 날짜를 누르면 달력으로 날짜를 지정할 수 있도록 Picker 지정
+        DatePickerDialog dpf = new DatePickerDialog(this, listener, year, month-1, day);
+        dpf.show();
+    }
+    //Picker 에 의해 설정된 날짜 적용
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int cyear, int cmonth, int cdayOfMonth) {
+            year = cyear;
+            month = cmonth+1;
+            day = cdayOfMonth;
+            setDayDate();
+            setDayTodolist();
+            setDayMemo();
+        }
+    };
 
-    //}
+    //todolist 항목이 없없어서 리스트뷰가 나타나지 않을 때, + 버튼과 배경을 누르면 일정 추가 화면으로 intent 하는 함수
+    public void intentTodo(View v) {
+        Intent todolistIntent = new Intent(DayActivity.this, DayTodolistActivity.class);
+        todolistIntent.putExtra("year", year);
+        todolistIntent.putExtra("month", month);
+        todolistIntent.putExtra("day", day);
+        startActivityForResult(todolistIntent, 1);
+    }
 
-    public void getCurrentDate_date() {
-        //long now = System.currentTimeMillis();
-        Date date = new Date();
-        SimpleDateFormat CurYear = new SimpleDateFormat("yyyy");
-        SimpleDateFormat CurMonth = new SimpleDateFormat("MM");
-        SimpleDateFormat CurDay = new SimpleDateFormat("dd");
-        year = Integer.parseInt(CurYear.format(date));
-        month = Integer.parseInt(CurMonth.format(date));
-        day = Integer.parseInt(CurDay.format(date));
+    @Override   //intent가 finish 된 뒤 실행. extra 값을 가져와 날짜를 바꾸고 그에 따라 daydate 와 todolist 내용을 바꿈.
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            year = data.getExtras().getInt("cYear");
+            month = data.getExtras().getInt("cMonth");
+            day = data.getExtras().getInt("cDay");
+            setDayDate();
+            setDayTodolist();
+            setDayMemo();
+        }
+    }
+
+    public void setDayTodolist() { //todolist 에 DB 내용에서 가져온 content 값을 가져옴.
+        int count = 0;
+        ImageView todoadd = (ImageView)findViewById(R.id.daytodo_add);
+        todolistLV = (ListView)findViewById(R.id.daytodo_list);
+        todolistAA.clearItem();
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor c = db.rawQuery("select _id, content, checked from pladaytodo_e where date = '"+ dd + "';", null);
+        while(c.moveToNext()) {
+            todolistAA.addItem(c.getInt(0), c.getString(1), c.getString(2));
+            count++;
+        }
+        if(count == 0 ){
+            todoadd.setVisibility(View.VISIBLE);
+        }
+        else todoadd.setVisibility(View.GONE);
+        c.close();
+        db.close();
+        todolistAA.notifyDataSetChanged();
+    }
+
+    public void setDayMemo() { //일기 화면에 DB에서 가져온 memocont 값을 가져옴.
+        dayMemo.setText("");
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor c = db.rawQuery("select dicont from pladaydi_ex where date = '"+ dd + "';", null);
+        while(c.moveToNext()) {
+            dayMemo.setText(c.getString(0));
+        }
+        c.close();
+        db.close();
     }
 }
